fd994d4c497767a0964ee15732d41150554e03cb
eshat002 <eshatjubayer22@outlook.com>
2024-08-15 03:00:09 +0600

    Add store.jsx and types.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,25 +7,29 @@ import ResetPasswordConfirm from "./screens/ResetPasswordConfirm";
 import Activate from "./screens/Activate";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/reset-password" element={<ResetPassword />} />
-          <Route
-            exact
-            path="/password/reset/confirm/:uid/:token"
-            element={<ResetPasswordConfirm />}
-          />
-          <Route exact path="activate/:ui/:token" element={<Activate />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/reset-password" element={<ResetPassword />} />
+            <Route
+              exact
+              path="/password/reset/confirm/:uid/:token"
+              element={<ResetPasswordConfirm />}
+            />
+            <Route exact path="activate/:ui/:token" element={<Activate />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </Provider>
   );
 };
 
