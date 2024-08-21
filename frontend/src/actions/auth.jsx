e6db2d1bdd0b1e0c79d5578e8a68c2a1967283aa
eshat002 <eshatjubayer22@outlook.com>
2024-08-21 19:10:17 +0600

    Add confirm_reset_password action and condition based login button on home

--- frontend/src/actions/auth.jsx
+++ frontend/src/actions/auth.jsx
@@ -138,6 +138,31 @@ export const password_reset = (email) => async (dispatch) => {
   }
 };
 
+export const password_reset_confirm =
+  (uid, token, new_password, re_new_password) => async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const body = JSON.stringify({ uid, token, new_password, re_new_password });
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_API_URL}/auth/users/reset_password_confirm/`,
+        body,
+        config
+      );
+
+      dispatch({
+        type: PASSWORD_RESET_CONFIRM_SUCCESS,
+      });
+    } catch (error) {
+      dispatch({
+        type: PASSWORD_RESET_CONFIRM_FAIL,
+      });
+    }
+  };
+
 export const logout = () => (dispatch) => {
   dispatch({
     type: LOGOUT,
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../actions/auth";
 
-const Home = () => {
+const Home = ({ isAuthenticated, logout }) => {
   return (
     <div className="home">
       <div className="bg-light p-5 rounded-lg m-3">
@@ -12,12 +14,21 @@ const Home = () => {
         </p>
         <hr className="my-4" />
         <p>Click the Login button to proceed </p>
-        <Link className="btn btn-dark btn-lg" to="/login" role="button">
-          Log In
-        </Link>
+        {isAuthenticated ? (
+          <Link className="btn btn-dark btn-lg" onClick={logout} role="button">
+            Logout
+          </Link>
+        ) : (
+          <Link className="btn btn-dark btn-lg" to="/login" role="button">
+            Log In
+          </Link>
+        )}
       </div>
     </div>
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+export default connect(mapStateToProps, { logout })(Home);
