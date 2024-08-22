604136af21ff9b68aaa2d4af1fcdec14f2e89b95
eshat002 <eshatjubayer22@outlook.com>
2024-08-22 20:12:47 +0600

    Add ResetPassword and ConfirmResetPassword component

--- frontend/src/screens/ResetPassword.jsx
+++ frontend/src/screens/ResetPassword.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+import { password_reset } from "../actions/auth";
 
-const ResetPassword = () => {
-  return <div>reset password</div>;
+const ResetPassword = ({ password_reset }) => {
+  const [RequestSent, setRequestSent] = useState(false);
+  const [FormData, setFormData] = useState({
+    email: "",
+  });
+  const { email } = FormData;
+  const onChange = (e) =>
+    setFormData({ ...FormData, [e.target.name]: e.target.value });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    password_reset(email);
+    setRequestSent(true);
+  };
+
+  if (RequestSent) {
+    return <Navigate to="/" />;
+  }
+  return (
+    <div className="container mt-5">
+      <h1>Reset Your Password</h1>
+
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="form-control mb-3"
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <button className="btn btn-dark" type="submit">
+          Reset Password
+        </button>
+      </form>
+    </div>
+  );
 };
 
-export default ResetPassword;
+export default connect(null, { password_reset })(ResetPassword);
--- frontend/src/screens/ResetPasswordConfirm.jsx
+++ frontend/src/screens/ResetPasswordConfirm.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import { password_reset_confirm } from "../actions/auth";
 
-const ResetPasswordConfirm = () => {
-  return <div>reset password confirm</div>;
+const ResetPasswordConfirm = ({ password_reset_confirm }) => {
+  const [RequestSent, setRequestSent] = useState(false);
+  const [FormData, setFormData] = useState({
+    new_password: "",
+    re_new_password: "",
+  });
+
+  const onChange = (e) =>
+    setFormData({ ...FormData, [e.target.name]: e.target.value });
+
+  const { new_password, re_new_password } = FormData;
+  const { uid, token } = useParams();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    password_reset_confirm(uid, token, new_password, re_new_password);
+    setRequestSent(true);
+  };
+
+  if (RequestSent) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div className="container mt-5">
+      <h1>Reset Your Password</h1>
+
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="password"
+            name="new_password"
+            placeholder="New Password"
+            className="form-control mb-3"
+            value={new_password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="re_new_password"
+            placeholder="Confirm New Password"
+            className="form-control mb-3"
+            value={re_new_password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <button className="btn btn-dark" type="submit">
+          Reset Password
+        </button>
+      </form>
+    </div>
+  );
 };
 
-export default ResetPasswordConfirm;
+export default connect(null, { password_reset_confirm })(ResetPasswordConfirm);
